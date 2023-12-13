4f00cfd832e0d99792cf521b51d290f0357269c4
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-12-14 10:12:55 +1300

    g

--- drf/test/urls.py
+++ drf/test/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from drf.test import views
+from .views import *
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -8,7 +9,8 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('test/user/', UserViewSet.as_view(), name="get_user_details"),
 ]
 
 urlpatterns += router.urls
