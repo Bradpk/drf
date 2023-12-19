33273bde9267df51cefcb7fb58b3206012367355
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-12-20 10:31:38 +1300

    g

--- drf/test/serializers.py
+++ drf/test/serializers.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+from .models import *
 
-class UserSerializer(serializers.ModelSerializer):
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = '__all__'  
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
--- drf/test/urls.py
+++ drf/test/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from drf.test import views
-from .views import *
+from . import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -9,8 +8,7 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('test/user/', UserViewSet.as_view(), name="get_user_details"),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 urlpatterns += router.urls
